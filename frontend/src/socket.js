bc29e620ed651d4f44ec3d9318311476437a1de1
ypavanr <ypavanreddy190@gmail.com>
2025-11-05 12:25:16 +0530

    url update

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,7 +1,7 @@
 // src/socket.js
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3000", {
+export const socket = io("https://multiplayer-wordle-pvla.onrender.com", {
   autoConnect: false,           // connect manually after login
   reconnection: true,
   reconnectionAttempts: 5,
